ef567ab83e9144eced1468c9683637223f8dbabc
Gabi <lucasgabrielgarcia1@gmail.com>
2024-05-19 20:01:19 -0300

    now test is saved in default datadir path

--- bioseq/management/commands/test.py
+++ bioseq/management/commands/test.py
@@ -32,46 +32,25 @@ class Command(BaseCommand):
 
         # Parse the GBK file into a SeqRecord object
         record = next(SeqIO.parse(h, "genbank"))
-        i= 0
-        for feature in record.features:
-            i += 1
-            
-            print(feature)
-            if i > 20:
-                break
-        # Extract the first 20 genes from the features of the SeqRecord
-        record.features = record.features[:101]
 
+        # Extract the first 20 genes from the features of the SeqRecord
+        record.features = record.features[:151]
+
+        ss = SeqStore(options["datadir"])
+        ss.create_idx_dir(accession)
+        test_folder = ss.db_dir(accession)
+        print(test_folder)
 
         # Define the output file path
-        output_file_path = os.path.join(options["datadir"], f"{accession}.gbk")
+        output_file_path = os.path.join(test_folder, f"{accession}.gbk")
 
-        # Write the new SeqRecord with only the first 20 genes to a new GBK file
+        # Write the new SeqRecord with only the first 100 genes to a new GBK file
         SeqIO.write(record, output_file_path, "genbank")
 
-        print(f"First 20 genes saved to {output_file_path}")
-
-
-        ## If asked, the content is printed as stdout and the file isn't saved
-        #try:
-
-        #    ss = SeqStore(options["datadir"])
-        #    test_folder =  ss.test()
-        #    print(test_folder)
-        #    if os.path.exists(test_folder):
-        #        shutil.rmtree(test_folder)
-
-            #Creates the test directory
-         #   os.makedirs(test_folder)
-            
-            # Creates the test_file
-         #   test_file = accession + '.gz'
-          #  with gzip.open(test_file, "wt") as hw:
-           #     shutil.copyfileobj(h, hw)
-
-           # shutil.move(test_file, test_folder)
-            
-
+        print(f"First 100 genes saved to {output_file_path}")
+        # Gzip the file
+        with open(output_file_path, 'rb') as f_in, gzip.open(output_file_path + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
         h.close()
 
